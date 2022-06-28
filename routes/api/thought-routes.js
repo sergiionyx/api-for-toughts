6a136c517ddd9e2c0055eb6e2038538477606af8
sergii-onyx <twilight.cympak@gmail.com>
2022-06-28 14:18:39 -0400

    thought routes completed

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const {
+  getAllThoughts,
+  getSingleThought,
+  addThought,
+  removeThought,
+  editThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thought-controller");
+
+// /api/thoughts/
+router.route("/").get(getAllThoughts);
+
+// /api/thoughts/<thoughtId>
+router
+  .route("/:id")
+  .get(getSingleThought)
+  .put(editThought)
+  .delete(removeThought);
+
+// /api/thoughts/<userId>
+router.route("/:userId").post(addThought);
+
+// /api/thoughts/<thoughtId>/reactions
+router.route("/:id/reactions").put(addReaction);
+
+router.route("/:id/:reactionId").put(removeReaction);
+
+module.exports = router;
